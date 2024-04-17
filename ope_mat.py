52fb5d5df72ace8f51f2fb0afa091eaa39601268
Giovanni Nogueira <79239919+giovanninog@users.noreply.github.com>
2024-04-17 20:10:05 +0000

    UPDATE

---
+++ ope_mat.py
@@ -0,0 +1,15 @@
+num1 = int(input("Digite um número: ")) 
+num2 = int(input("Digite outro número: "))
+
+operacao = input("Digite uma operação que deseja realizar (+ - * /): ")
+
+if operacao == '+':
+    print(num1 + num2)
+elif operacao == '-':
+    print(abs(num1 - num2))
+elif operacao == '*':
+    print(num1 * num2)
+elif operacao == '/':
+    print(num1 / num2)
+else:
+    print("Operação Inválida, tente novamente!")
